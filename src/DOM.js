3c11e858e5649df0e6d36f89387dc798d45d3e81
Borrispooman <freddyroberts69420@gmail.com>
2025-07-22 21:28:38 +1000

    DOM.renderHome() progress

---
+++ src/DOM.js
@@ -0,0 +1,46 @@
+
+//import STORAGE from "./STORAGE.js"; 
+
+const DOM = (function(){
+
+	const renderHome = function(){
+		const mainContent = document.querySelector(".main-content");
+		const homeTodos = [];     //STORAGE.loadHomeTodos(); 	//this should return an empty list if theres no todos in local storage, then the "add-todo" button will be appended to the top.
+		for (let i = 0; i < homeTodos.length; i++) {
+			const parentTodoDiv = document.createElement("div");
+			parentTodoDiv.id = `${homeTodos[i].id}`;
+			parentTodoDiv.className = "todo-div";
+			const titleDiv = document.createElement("div"); 
+			titleDiv.textContent = `${homeTodos[i].title}`;
+			const checkButton = document.createElement("button");
+			checkButton.className = "check-button"
+			const dateDiv = document.createElement("div");
+			dateDiv.textContent = `${homeTodos[i].dueDate}`
+			parentTodoDiv.append(titleDiv, checkButton, dateDiv);
+			mainContent.append(parentTodoDiv);
+		};		
+
+		const checkButtons = document.querySelectorAll(".check-button");
+		checkButtons.forEach((button) => {
+			button.addEventListener("click", function(event) {
+				const clickedButton = event.target;
+  			const selectedTodo = clickedButton.parentElement;
+				if (selectedTodo) {
+					selectedTodo.remove();
+					STORAGE.removeTodo(`${selectedTodo.id}`);
+				};
+			});
+		});
+
+		const addTodoDiv = document.createElement("button");
+		addTodoDiv.textContent = "New Todo";
+		addTodoDiv.onclick = function() {
+			document.querySelector("dialog").showModal();
+		};
+		mainContent.append(addTodoDiv);
+	};
+	
+	return {renderHome};
+})();
+
+export default DOM;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./style.css";
 import "./reset.css";
+import { createTodo } from "./TODO.js";
+import DOM from "./DOM.js";
+
+const homeTab = document.querySelector("#homeTab");
+homeTab.addEventListener("click", function() {
+	DOM.renderHome();
+});
+
 console.log("hello, world");
+
+const newTodo = createTodo("jirk off", "masturbate only once a week", "Daily", "Mild");
+console.log(newTodo);
